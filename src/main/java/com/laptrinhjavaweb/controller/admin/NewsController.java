ac6b609db097caae89ec423ec990962570c01a30
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-02-20 12:38:09 +0700

    Setup new template

--- src/main/java/com/laptrinhjavaweb/controller/admin/NewsController.java
+++ src/main/java/com/laptrinhjavaweb/controller/admin/NewsController.java
@@ -1,20 +1,24 @@
 package com.laptrinhjavaweb.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.laptrinhjavaweb.model.NewModel;
+
 @Controller(value = "newsControllerOfAdmin")
 public class NewsController {
 	
-	@RequestMapping(value = "/news/list", method = RequestMethod.GET)
-	public ModelAndView showList() {
-		ModelAndView mav = new ModelAndView("admin/news/list");
+	@RequestMapping(value = "/quan-tri/bai-viet/danh-sach", method = RequestMethod.GET)
+	public ModelAndView showList(@ModelAttribute("model") NewModel  model) {
+ 		ModelAndView mav = new ModelAndView("admin/news/list");
+ 		mav.addObject("model", model);
 		return mav;
 	}
 	
-	@RequestMapping(value = "/news/edit", method = RequestMethod.GET)
+	@RequestMapping(value = "/quan-tri/bai-viet/chinh-sua", method = RequestMethod.GET)
 	public ModelAndView editNews() {
 		ModelAndView mav = new ModelAndView("admin/news/edit");
 		return mav;
